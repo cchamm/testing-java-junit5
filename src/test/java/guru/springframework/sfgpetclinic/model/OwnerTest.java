ff19a743ed03c97c25a4f2f9d2a9b2d17e91f061
Cham <runningpaipai@gmail.com>
2024-02-16 23:44:38 +0800

    Feat: Create Parameterized test in the Owner Test using csv as source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -46,6 +47,16 @@ class OwnerTest implements ModelTests {
         System.out.println(ownerType);
     }
 
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "FL,1,1",
+            "OH,2,2",
+            "BB,3,3"
+    })
+    void csvTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
 
 
 }
