76aafd0fb02bd7058ec5c334af6db3ca55c1a98f
Cham <runningpaipai@gmail.com>
2024-02-16 16:13:14 +0800

    Feat: Grouped Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        // given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe"),
+                () -> assertEquals(person.getLastName(), "Buck"));
+    }
+
+
+    @Test
+    void groupedAssertionsMsgs() {
+        // given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe2", "First name failed"),
+                () -> assertEquals(person.getLastName(), "Buckxx", "Last Name Failed"));
+    }
+
+}
