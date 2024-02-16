3b280746addf0b112e74c95b3a64319ca31282e3
Cham <runningpaipai@gmail.com>
2024-02-16 18:38:48 +0800

    Chore: Disable a unit test in the IndexControllerTest

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -64,6 +64,7 @@ class IndexControllerTest implements ControllerTests {
         });
     }
 
+    @Disabled
     @Test
     void testAssumptions() {
         assumeTrue("GURU".equals(System.getenv("GURU_RUNTIME")));
