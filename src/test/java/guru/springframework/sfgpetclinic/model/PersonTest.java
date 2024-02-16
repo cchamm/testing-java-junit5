695def1eddde5caf0d8e345b9aa6b36194e269be
Cham <runningpaipai@gmail.com>
2024-02-16 18:23:42 +0800

    Feat: Tag Owner Test and Person Test with "Model"

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Tag("model")
 class PersonTest {
 
     @Test
