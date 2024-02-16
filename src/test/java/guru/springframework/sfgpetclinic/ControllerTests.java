4046a705d4b80b9fb017bca9de28f4b6109a66e9
Cham <runningpaipai@gmail.com>
2024-02-16 18:38:32 +0800

    Feat: Use common interface to decorate different test 2 - define before all

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgpetclinic;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("controller")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTests {
 
-
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("Lets do something here");
+    }
 }
