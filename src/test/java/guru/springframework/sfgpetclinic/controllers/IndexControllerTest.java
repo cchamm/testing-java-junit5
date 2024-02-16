9db29bd8e2253427dd6250b85b1a04efc09f7be2
Cham <runningpaipai@gmail.com>
2024-02-16 16:36:39 +0800

    Feat: JUnit conditional test

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import jdk.jfr.Enabled;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -56,4 +59,55 @@ class IndexControllerTest {
             System.out.println("I got here 23434 ");
         });
     }
+
+    @Test
+    void testAssumptions() {
+        assumeTrue("GURU".equals(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionsTrueAssumptionIsTrue() {
+        assumeTrue("GURU".equals("GURU"));
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testMeOnMacOS() {
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testMeOnLinux() {
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testMeOnWindows() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+
+    }
+
+    @EnabledOnJre(JRE.OTHER)
+    @Test
+    void testMeOnJavaOther() {
+
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "jt")
+    @Test
+    void testIfUserJT() {
+
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "fred")
+    @Test
+    void testIfUserFred() {
+
+    }
+
+
 }
